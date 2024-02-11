ad3aa758ad90de66da2262f773f96f323ede6cff
Martin Imre <martinimre25@gmail.com>
2024-02-11 17:35:52 +0100

    fix(selection): Allow range selection and fix related bugs
    Previously range selection wasn't allowed.
    This commit adds the range selection, as well as fixes bugs related to
    selections.

--- plugin/code-link.lua
+++ plugin/code-link.lua
@@ -2,11 +2,17 @@ local code_link = require("code-link")
 local clipboard = require("code-link.clipboard")
 
 vim.api.nvim_create_user_command("CodeLink", function(args)
-	local branch_name = args.args
-	local link = code_link.link()
-	local generated_link = link:create_link(branch_name)
+    local branch_name = args.args
+    local link = code_link.link()
+    local generated_link = link:create_link(branch_name)
 
-	clipboard.copy_or(generated_link)
+    clipboard.copy_or(generated_link)
 
-	vim.notify(generated_link)
-end, { nargs = "*" })
+    vim.notify(generated_link)
+
+    -- we use the marks '< and '> to find the begin and ending of a visuall
+    -- selection. Thus we need to delete them or otherwise a single line
+    -- :CodeLink (without visual selection) will result in a link with the
+    -- lines from the previous selection
+    vim.cmd("delmarks <>")
+end, { nargs = "*", range = "%" })

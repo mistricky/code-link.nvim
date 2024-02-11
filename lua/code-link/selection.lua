97ed8983053f18f04662fe94b7063ad141f323d0
Martin Imre <martinimre25@gmail.com>
2024-02-11 17:35:52 +0100

    fix(selection): Allow range selection and fix related bugs
    Previously range selection wasn't allowed.
    This commit adds the range selection, as well as fixes bugs related to
    selections.

--- lua/code-link/selection.lua
+++ lua/code-link/selection.lua
@@ -1,31 +1,31 @@
 local Selection = {}
 
 local function wrap_selection_value(value)
-	return value, value
+    return value, value
 end
 
 local function get_selection()
-	local line_start = vim.fn.line("'<")
-	local line_end = vim.fn.line("'>")
+    local line_start = vim.fn.line("'<")
+    local line_end = vim.fn.line("'>")
 
-	if line_start == line_end then
-		local row, _ = table.unpack(vim.api.nvim_win_get_cursor(0))
+    if line_start == line_end then
+        local pos = vim.api.nvim_win_get_cursor(0)
+        local row = pos[1]
+        return wrap_selection_value(row)
+    end
 
-		return wrap_selection_value(row)
-	end
-
-	return line_start, line_end
+    return line_start, line_end
 end
 
 function Selection.new()
-	local line_start, line_end = get_selection()
-	local instance = { line_start = line_start, line_end = line_end }
+    local line_start, line_end = get_selection()
+    local instance = { line_start = line_start, line_end = line_end }
 
-	setmetatable(instance, {
-		__index = Selection,
-	})
+    setmetatable(instance, {
+        __index = Selection,
+    })
 
-	return instance
+    return instance
 end
 
 return Selection

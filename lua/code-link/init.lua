ebecb7339de86ca47b1a0e4fc5b30f9a8132d4ab
Mist <mist.zzh@gmail.com>
2024-02-12 19:39:53 +0800

    [Feat] add default config & table utils

--- lua/code-link/init.lua
+++ lua/code-link/init.lua
@@ -1,9 +1,10 @@
 local CodeLink = require("code-link.link")
 local static = require("code-link.static")
+local table_utils = require("code-link.utils.table")
 local main = {}
 
 function main.setup(config)
-	static.config = config
+	static.config = table_utils.merge(static.config, config)
 end
 
 function main.link()
